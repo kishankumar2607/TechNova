57a742f2d7976f5b5eed461a636072f3e10c46f4
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-01 00:11:47 -0400

    Create Product.cs

---
+++ TechNova/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace TechNova.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQty { get; set; }
+        public string? ImageURL { get; set; }
+        public decimal AvgRating { get; set; }
+        public int ReviewCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
