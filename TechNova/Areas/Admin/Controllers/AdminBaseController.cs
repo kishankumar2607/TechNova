40f03b3e52eaa8e0c4ed9545de820524c7916b87
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-01 23:57:07 -0400

    Create AdminBaseController.cs

---
+++ TechNova/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechNova.Areas.Admin.Controllers
+{
+    public class AdminBaseController : Controller
+    {
+        protected bool IsAdmin()
+        {
+            return HttpContext.Request.Cookies["Role"] == "Admin";
+        }
+
+        protected IActionResult RedirectIfNotAdmin()
+        {
+            if (!IsAdmin())
+            {
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+            return null;
+        }
+    }
+}
