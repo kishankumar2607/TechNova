7bab698df5086a271a2962d1c9bd59d2e9859bd7
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-02 01:34:34 -0400

    Create AccountController.cs

---
+++ TechNova/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechNova.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("UserName");
+            Response.Cookies.Delete("UserEmail");
+            Response.Cookies.Delete("UserId");
+            Response.Cookies.Delete("Role");
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
