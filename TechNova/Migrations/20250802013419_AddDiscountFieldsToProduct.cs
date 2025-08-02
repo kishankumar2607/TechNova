54aea946dccfa57b619b27def08fdfa5f642c468
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-01 23:57:50 -0400

    Add discount fields to Product and update migration

---
+++ TechNova/Migrations/20250802013419_AddDiscountFieldsToProduct.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechNova.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscountFieldsToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountPercent",
+                table: "Products",
+                type: "decimal(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountedPrice",
+                table: "Products",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountPercent",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountedPrice",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
