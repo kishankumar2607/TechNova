a91ca3073fbcff465bf40eaa738dd995628fe55a
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-10 03:10:06 -0400

    Create Order.cs

---
+++ TechNova/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechNova.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public string BillingName { get; set; }
+        public string CompanyName { get; set; }
+        public string StreetAddress { get; set; }
+        public string Apartment { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+        public int PaymentID { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalAmount { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; }
+    }
+}
