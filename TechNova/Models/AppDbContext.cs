409d0adf766864b80525935da53bd04f093a81eb
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-10 03:10:23 -0400

    Update AppDbContext.cs

--- TechNova/Models/AppDbContext.cs
+++ TechNova/Models/AppDbContext.cs
@@ -9,6 +9,9 @@ namespace TechNova.Models
 
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
