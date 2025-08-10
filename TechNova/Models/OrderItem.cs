91a3cd3aa78357427c12721ca75f593e62232c88
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-10 03:10:03 -0400

    Create OrderItem.cs

---
+++ TechNova/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechNova.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemID { get; set; }
+
+        public int OrderID { get; set; }
+        [ForeignKey(nameof(OrderID))]
+        public Order Order { get; set; }
+
+        public int ProductID { get; set; }
+        [ForeignKey(nameof(ProductID))]
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal UnitPrice { get; set; }
+    }
+}
