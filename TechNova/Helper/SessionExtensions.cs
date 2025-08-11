4fdf9c4f1e78a23125993c897a0aab070a081eaa
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-11 00:55:42 -0400

    Create SessionExtensions.cs

---
+++ TechNova/Helper/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace TechNova.Helper
+{
+    public static class SessionExtensions
+    {
+        private static readonly JsonSerializerOptions _opts = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false
+        };
+
+        public static void SetObject<T>(this ISession session, string key, T value)
+            => session.SetString(key, JsonSerializer.Serialize(value, _opts));
+
+        public static T? GetObject<T>(this ISession session, string key)
+        {
+            var json = session.GetString(key);
+            return string.IsNullOrEmpty(json) ? default : JsonSerializer.Deserialize<T>(json, _opts);
+        }
+    }
+}
