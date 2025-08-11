2d6f36f0b7ccb32591d314b73d6ba5fc5b376f94
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-11 00:55:35 -0400

    Create CartItem.cs

---
+++ TechNova/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TechNova.Models
+{
+    public class CartItem
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; } = "";
+        public string? ImageURL { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Qty { get; set; }
+        public decimal LineTotal => UnitPrice * Qty;
+    }
+}
