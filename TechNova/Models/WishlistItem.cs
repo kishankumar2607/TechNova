1edcf3abbcb2060dcb800e1164ff26594cb60ad5
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-08-13 01:40:51 -0400

    Create WishlistItem.cs

---
+++ TechNova/Models/WishlistItem.cs
@@ -0,0 +1,14 @@
+﻿namespace TechNova.Models
+{
+    public class WishlistItem
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; } = "";
+        public string? ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public decimal? DiscountedPrice { get; set; }
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal EffectivePrice => DiscountedPrice ?? Price;
+    }
+}
